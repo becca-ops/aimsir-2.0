d2232364c9558515649a0def338966ccdd2de002
becca-ops <rebecca.a.laffan@ul.ie>
2024-09-18 14:15:26 +0100

    Added days in forecast

--- src/index.js
+++ src/index.js
@@ -56,6 +56,13 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "e70bdao89402f423202bat5b1ea3dcfc";
   let apiURL = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -69,12 +76,13 @@ function displayForecast(response) {
 
   let forecastHtml = "";
 
-  response.data.daily.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
   <div class="weather-forecast-day">
-<div class="weather-forecast-date">Tue</div>
+<div class="weather-forecast-date">${formatDay(day.time)}</div>
 
 <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
 <div class="weather-forecast-temperatures">
@@ -86,6 +94,7 @@ function displayForecast(response) {
     </div>  
             </div>
             `;
+    }
   });
 
   forecastElement.innerHTML = forecastHtml;
